6f8b672d3bc9cab2c5d7d3d30250e4fa83708e97
mxasraful <mxasraful@outlook.com>
2022-01-01 01:26:46 +0600

    Bug Fix

--- src/Components/Admin/AdminHeader/AdminHeader.js
+++ src/Components/Admin/AdminHeader/AdminHeader.js
@@ -16,13 +16,13 @@ const AdminHeader = () => {
                     </button>
                     <div class="collapse navbar-collapse" id="navbarNavDropdown">
                         <div class="">
-                            <form class="d-flex input-group mb-3 admin-search-form pt-3">
-                                <span class="input-group-text" id="basic-addon1">
+                            <form class="d-flex input-group mb-3 ms-5 admin-search-form pt-3 header-search-form">
+                                <span class="input-group-text header-search-icon-item" id="basic-addon1">
                                     <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-search" viewBox="0 0 16 16">
                                         <path d="M11.742 10.344a6.5 6.5 0 1 0-1.397 1.398h-.001c.03.04.062.078.098.115l3.85 3.85a1 1 0 0 0 1.415-1.414l-3.85-3.85a1.007 1.007 0 0 0-.115-.1zM12 6.5a5.5 5.5 0 1 1-11 0 5.5 5.5 0 0 1 11 0z" />
                                     </svg>
                                 </span>
-                                <input type="search" class="form-control" placeholder="Serch..." />
+                                <input type="search" class="form-control header-search-input" placeholder="Search..." />
                             </form>
                         </div>
                         <ul className="ms-auto navbar-nav admin-header-nav-items">
